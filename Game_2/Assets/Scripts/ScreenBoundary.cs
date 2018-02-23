0e517b7c43b58f5d4b7854df3c1d3880add2a8dc
Shuzhan Xie <swqa7697@gmail.com>
2018-02-22 19:55:59 -0800

    Some Little Changes
    Bullet Color: Orange
    Bullet will be destroyed when leaving the edge of the screen.

---
+++ Game_2/Assets/Scripts/ScreenBoundary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBoundary: MonoBehaviour {
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+            Destroy(other.gameObject);
+    }
+}
